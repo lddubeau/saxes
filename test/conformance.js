b6640900c4a639c9f7be0fed4c72bf0ecaa117fa
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-07-05 14:30:46 -0400

    build: skip more tests that are out of scope

--- test/conformance.js
+++ test/conformance.js
@@ -16,6 +16,65 @@ const saxes = require("../lib/saxes");
 //
 
 const SKIP = {
+  "not-wf-sa-056": "DTD",
+  "not-wf-sa-078": "DTD",
+  "not-wf-sa-079": "DTD",
+  "not-wf-sa-080": "DTD",
+  "not-wf-sa-084": "DTD",
+  "not-wf-sa-113": "DTD",
+  "not-wf-sa-114": "DTD",
+  "not-wf-sa-121": "DTD",
+  "not-wf-sa-128": "DTD",
+  "not-wf-sa-149": "DTD",
+  "not-wf-sa-160": "ENTITIES",
+  "not-wf-sa-161": "ENTITIES",
+  "not-wf-sa-162": "ENTITIES",
+  "not-wf-sa-180": "ENTITIES",
+  "valid-sa-023": "ENTITIES",
+  "valid-sa-024": "ENTITIES",
+  "valid-sa-053": "ENTITIES",
+  "valid-sa-066": "ENTITIES",
+  "valid-sa-085": "ENTITIES",
+  "valid-sa-089": "ENTITIES",
+  "valid-sa-108": "ENTITIES",
+  "valid-sa-110": "ENTITIES",
+  "valid-sa-114": "ENTITIES",
+  "valid-sa-115": "ENTITIES",
+  sa02: "DTD",
+  pi: "DTD",
+  "o-p43pass1": "ENTITIES",
+  "o-p68pass1": "ENTITIES",
+  "o-p09fail3": "DTD",
+  "o-p12fail1": "DTD",
+  "o-p12fail2": "DTD",
+  "o-p12fail3": "DTD",
+  "o-p12fail4": "DTD",
+  "o-p12fail5": "DTD",
+  "o-p12fail6": "DTD",
+  "o-p12fail7": "DTD",
+  "o-p29fail1": "DTD",
+  "o-p69fail1": "DTD",
+  "o-p69fail2": "DTD",
+  "o-p69fail3": "DTD",
+  "ibm-not-wf-P28-ibm28n01.xml": "DTD",
+  "ibm-not-wf-P28-ibm28n02.xml": "DTD",
+  "ibm-not-wf-P28-ibm28n03.xml": "DTD",
+  "ibm-not-wf-P28-ibm28n04.xml": "DTD",
+  "ibm-not-wf-P28-ibm28n05.xml": "DTD",
+  "ibm-not-wf-P28-ibm28n06.xml": "DTD",
+  "ibm-not-wf-P29-ibm29n01.xml": "DTD",
+  "ibm-not-wf-P29-ibm29n02.xml": "DTD",
+  "ibm-not-wf-P29-ibm29n03.xml": "DTD",
+  "ibm-not-wf-P29-ibm29n04.xml": "DTD",
+  "ibm-not-wf-P29-ibm29n05.xml": "DTD",
+  "ibm-not-wf-P29-ibm29n06.xml": "DTD",
+  "ibm-not-wf-P29-ibm29n07.xml": "DTD",
+  "ibm-not-wf-P66-ibm66n01.xml": "DTD",
+  "ibm-not-wf-P66-ibm66n03.xml": "DTD",
+  "ibm-not-wf-P66-ibm66n05.xml": "DTD",
+  "ibm-not-wf-P66-ibm66n07.xml": "DTD",
+  "ibm-not-wf-P66-ibm66n09.xml": "DTD",
+  "ibm-not-wf-P66-ibm66n11.xml": "DTD",
   "ibm-not-wf-P68-ibm68n07.xml": "DTD",
   "ibm-not-wf-P69-ibm69n01.xml": "DTD",
   "ibm-not-wf-P69-ibm69n02.xml": "DTD",
@@ -56,16 +115,11 @@ class SaxesSelection extends Selection {
   shouldSkipTest(test) {
     return Promise.resolve()
       .then(() => SKIP[test.id] ||
-            //
-            // Section 4.2 deals with ENTITY declarations which
-            // appear in DTDs.
-            //
-            // Section 4.2.2 deals with NDATA which appears in DTDs.
-            //
-            // Section 4.7 deals with NOTATION declarations. These
-            // appear in DTDs and we don't parse them.
-            //
-            test.includesSections(["4.2", "4.2.2", "4.7"]) ||
+            // These sections are excluded because they require
+            // parsing DTDs.
+            test.includesSections(
+              ["[12]", "[13]", "[69]", "3.2", "3.2.1", "3.2.2", "3.3",
+               "3.3.1", "3.3.2", "4.2", "4.2.2", "4.5", "4.7"]) ||
             super.shouldSkipTest(test));
   }
 }
