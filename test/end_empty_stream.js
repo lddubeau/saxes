f2de52026f8d4326b2b5029e1019071a68bbb9b7
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-07-05 18:10:51 -0400

    fix: raise an error if the document does not have a root

--- test/end_empty_stream.js
+++ test/end_empty_stream.js
@@ -1,9 +1,11 @@
 "use strict";
 
+const { expect } = require("chai");
 const saxes = require("../lib/saxes");
 
 it("end empty stream", () => {
   const saxesStream = saxes.createStream();
   // It musn't throw.
-  saxesStream.end();
+  expect(() => saxesStream.end()).to.throw(Error,
+                                           /Document must contain a root element/);
 });
