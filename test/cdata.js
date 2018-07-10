95d192f888c3cbacc0d133ad4328cf5601031f85
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-07-10 07:09:15 -0400

    fix: emit CDATA on empty CDATA section too

--- test/cdata.js
+++ test/cdata.js
@@ -2,11 +2,12 @@
 
 require(".").test({
   name: "cdata",
-  xml: "<r><![CDATA[ this is character data  ]]></r>",
+  xml: "<r><![CDATA[ this is character data  ]]><![CDATA[]]></r>",
   expect: [
     ["opentagstart", { name: "r", attributes: {} }],
     ["opentag", { name: "r", attributes: {}, isSelfClosing: false }],
     ["cdata", " this is character data  "],
+    ["cdata", ""],
     ["closetag", "r"],
   ],
 });
