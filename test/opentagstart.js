bb2085ccfd1c86aae164fc7e0bc810bdbeba848f
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-08-14 21:06:35 -0400

    perf: simplify captureWhile
    Notably the atStart parameter is no longer computed and passed to the
    test. Those places that need it need to compute it themselves.
    
    In fact most places that used it don't need it. The ``isNameStartChar`` checks
    were mostly redundant as the necessary check on the first character of names was
    done elsewhere.

--- test/opentagstart.js
+++ test/opentagstart.js
@@ -1,93 +1,97 @@
 "use strict";
 
-require(".").test({
-  name: "openstarttag (xmlns true)",
-  xml: "<root length='12345'></root>",
-  expect: [
-    [
-      "opentagstart",
-      {
-        name: "root",
-        ns: {},
-        attributes: {},
-      },
-    ],
-    [
-      "opentag",
-      {
-        name: "root",
-        prefix: "",
-        local: "root",
-        uri: "",
-        attributes: {
-          length: {
-            name: "length",
-            value: "12345",
-            prefix: "",
-            local: "length",
-            uri: "",
-          },
-        },
-        ns: {},
-        isSelfClosing: false,
-      },
-    ],
-    [
-      "closetag",
-      {
-        name: "root",
-        prefix: "",
-        local: "root",
-        uri: "",
-        attributes: {
-          length: {
-            name: "length",
-            value: "12345",
-            prefix: "",
-            local: "length",
-            uri: "",
-          },
-        },
-        ns: {},
-        isSelfClosing: false,
-      },
-    ],
-  ],
-  opt: {
-    xmlns: true,
-  },
-});
+const { test } = require(".");
 
-require(".").test({
-  name: "openstarttag (xmlns false)",
-  xml: "<root length='12345'></root>",
-  expect: [
-    [
-      "opentagstart",
-      {
-        name: "root",
-        attributes: {},
-      },
-    ],
-    [
-      "opentag",
-      {
-        name: "root",
-        attributes: {
-          length: "12345",
+describe("openstarttag", () => {
+  test({
+    name: "good name, xmlns true",
+    xml: "<root length='12345'></root>",
+    expect: [
+      [
+        "opentagstart",
+        {
+          name: "root",
+          ns: {},
+          attributes: {},
         },
-        isSelfClosing: false,
-      },
-    ],
-    [
-      "closetag",
-      {
-        name: "root",
-        attributes: {
-          length: "12345",
+      ],
+      [
+        "opentag",
+        {
+          name: "root",
+          prefix: "",
+          local: "root",
+          uri: "",
+          attributes: {
+            length: {
+              name: "length",
+              value: "12345",
+              prefix: "",
+              local: "length",
+              uri: "",
+            },
+          },
+          ns: {},
+          isSelfClosing: false,
         },
-        isSelfClosing: false,
-      },
+      ],
+      [
+        "closetag",
+        {
+          name: "root",
+          prefix: "",
+          local: "root",
+          uri: "",
+          attributes: {
+            length: {
+              name: "length",
+              value: "12345",
+              prefix: "",
+              local: "length",
+              uri: "",
+            },
+          },
+          ns: {},
+          isSelfClosing: false,
+        },
+      ],
     ],
-  ],
+    opt: {
+      xmlns: true,
+    },
+  });
+
+  test({
+    name: "good name, xmlns false",
+    xml: "<root length='12345'></root>",
+    expect: [
+      [
+        "opentagstart",
+        {
+          name: "root",
+          attributes: {},
+        },
+      ],
+      [
+        "opentag",
+        {
+          name: "root",
+          attributes: {
+            length: "12345",
+          },
+          isSelfClosing: false,
+        },
+      ],
+      [
+        "closetag",
+        {
+          name: "root",
+          attributes: {
+            length: "12345",
+          },
+          isSelfClosing: false,
+        },
+      ],
+    ],
+  });
 });
