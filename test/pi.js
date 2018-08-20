83d2b610f7c0c628537b1754a145ce5b0edfef47
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-08-20 14:14:29 -0400

    fix: use `isNameChar` for later chars in PI target
    We used `isNameStartChar`, which is too restrictive and would cause false
    well-formedness errors.

---
+++ test/pi.js
@@ -0,0 +1,42 @@
+"use strict";
+
+const { test } = require(".");
+
+describe("processing instructions", () => {
+  test({
+    name: "with well-formed name",
+    xml: "<?xml-stylesheet version='1.0'?><foo/>",
+    expect: [
+      ["processinginstruction", {
+        target: "xml-stylesheet",
+        body: "version='1.0'",
+      }],
+      ["opentagstart", {
+        name: "foo",
+        attributes: {},
+        ns: {},
+      }],
+      ["opentag", {
+        name: "foo",
+        local: "foo",
+        prefix: "",
+        uri: "",
+        attributes: {},
+        ns: {},
+        isSelfClosing: true,
+      }],
+      ["closetag", {
+        name: "foo",
+        local: "foo",
+        prefix: "",
+        uri: "",
+        attributes: {},
+        ns: {},
+        isSelfClosing: true,
+      }],
+    ],
+    opt: {
+      xmlns: true,
+    },
+  });
+});
