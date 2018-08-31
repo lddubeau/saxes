d7b1abea6c5ce8826757aacdca24ed6e289f1c19
Louis-Dominique Dubeau <ldd@lddubeau.com>
2018-08-31 14:37:49 -0400

    fix: normalize \r\n and \r followed by something else to \n
    Closes #2

---
+++ test/eol-handling.js
@@ -0,0 +1,58 @@
+"use strict";
+
+const { test } = require(".");
+
+/* eslint-disable linebreak-style */
+const xml = `\
+<?xml version="1.0" encoding="utf-8"?>
+
+<moo a="12
+        3"
+     >
+  abc
+  def&#xD;
+  ghi
+  xxxx
+</moo>
+`;
+/* eslint-enable linebreak-style */
+
+const expect = [
+  ["text", "\n\n"],
+  ["opentagstart", { name: "moo", attributes: {} }],
+  ["opentag", {
+    name: "moo",
+    attributes: {
+      a: "12\n        3",
+    },
+    isSelfClosing: false,
+  }],
+  ["text", "\n  abc\n  def\r\n  ghi\n\n  xx\nxx\n"],
+  ["closetag", {
+    name: "moo",
+    attributes: {
+      a: "12\n        3",
+    },
+    isSelfClosing: false,
+  }],
+  ["text", "\n"],
+];
+
+describe("eol handling", () => {
+  test({
+    name: "one chunk",
+    xml,
+    expect,
+  });
+
+  test({
+    name: "char-by-char",
+    expect,
+    fn(parser) {
+      for (const x of xml) {
+        parser.write(x);
+      }
+      parser.close();
+    },
+  });
+});
--- test/parser-position.js
+++ test/parser-position.js
@@ -7,14 +7,19 @@ const { test } = require(".");
 function testPosition(name, chunks, expectedEvents) {
   it(name, () => {
     const parser = new saxes.SaxesParser();
-    for (const expectation of expectedEvents) {
-      parser[`on${expectation[0]}`] = function handler() {
+    let expectedIx = 0;
+    for (const ev of saxes.EVENTS) {
+      // eslint-disable-next-line no-loop-func
+      parser[`on${ev}`] = () => {
+        const expectation = expectedEvents[expectedIx++];
+        expect(expectation[0]).to.equal(ev);
         // eslint-disable-next-line guard-for-in
         for (const prop in expectation[1]) {
           expect(parser[prop]).to.deep.equal(expectation[1][prop]);
         }
       };
     }
+
     for (const chunk of chunks) {
       parser.write(chunk);
     }
@@ -38,6 +43,19 @@ describe("parser position", () => {
       ["opentag", { position: 5 }],
       ["text", { position: 19 }],
       ["closetag", { position: 19 }],
+    ]);
+
+  testPosition(
+    "with various newlines",
+    ["<div>abcde\r\n<foo/>f\rgh</div>"], [
+      ["opentagstart", { position: 5, line: 1, column: 5 }],
+      ["opentag", { position: 5, line: 1, column: 5 }],
+      ["text", { position: 17, line: 2, column: 5 }],
+      ["opentagstart", { position: 17, line: 2, column: 5 }],
+      ["opentag", { position: 18, line: 2, column: 6 }],
+      ["closetag", { position: 18, line: 2, column: 6 }],
+      ["text", { position: 28, line: 3, column: 8 }],
+      ["closetag", { position: 28, line: 3, column: 8 }],
     ]);
 
   test({
