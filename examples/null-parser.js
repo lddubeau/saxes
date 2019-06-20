28845a0eb6d8175a3636e875de7c1f9b99d8b4ec
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-06-20 11:08:36 -0400

    refactor: allow running null-parser with a set buffer size

--- examples/null-parser.js
+++ examples/null-parser.js
@@ -1,16 +1,57 @@
 "use strict";
 
+/* eslint-disable no-console */
+
 const fs = require("fs");
 const saxes = require("../lib/saxes");
 
-const xml = fs.readFileSync(process.argv[2]);
-const start = Date.now();
-const parser = new saxes.SaxesParser({ xmlns: true });
+const { argv } = process;
+let bs;
+const first = argv[2];
+let filePath = first;
+if (first.startsWith("--bs=")) {
+  bs = Number(first.substring(first.indexOf("=") + 1));
+  if (Number.isNaN(bs)) {
+    throw new Error("bs is not a number");
+  }
+  // eslint-disable-next-line prefer-destructuring
+  filePath = argv[3];
+}
 
-parser.onerror = (err) => {
-  console.error(err);
-};
+if (bs === undefined) {
+  const xml = fs.readFileSync(filePath);
+  const start = Date.now();
+  const parser = new saxes.SaxesParser({ xmlns: true });
+  parser.onerror = (err) => {
+    console.error(err);
+  };
 
-parser.write(xml);
-parser.close();
-console.log(`Parsing time: ${Date.now() - start}`);
+  parser.write(xml);
+  parser.close();
+  console.log(`Parsing time: ${Date.now() - start}`);
+}
+else {
+  const input = fs.createReadStream(filePath);
+  const start = Date.now();
+  const parser = new saxes.SaxesParser({ xmlns: true });
+  parser.onerror = (err) => {
+    console.error(err);
+  };
+
+  input.on("readable", () => {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      const chunk = input.read(bs);
+      if (chunk === null) {
+        return;
+      }
+
+      parser.write(chunk);
+    }
+  });
+
+  input.on("end", () => {
+    parser.close();
+    console.log(`Parsing time: ${Date.now() - start}`);
+  });
+}
