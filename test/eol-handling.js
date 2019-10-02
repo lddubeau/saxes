7b3db7596ff581d719d0d02ca0f4a7ddbff2ea77
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-10-02 06:28:42 -0400

    fix: fix bug with initial eol characters
    The previous code incorrectly handled eol characters at the very start of a
    file. This has been fixed.

--- test/eol-handling.js
+++ test/eol-handling.js
@@ -2,8 +2,10 @@
 
 const { test } = require(".");
 
-/* eslint-disable linebreak-style */
-const xml = `\
+describe("eol handling", () => {
+  describe("mixed", () => {
+    /* eslint-disable linebreak-style */
+    const xml = `\
 <?xml version="1.0" encoding="utf-8"?>
 
 <moo a="12
@@ -15,44 +17,108 @@ const xml = `\
   xxxx
 </moo>
 `;
-/* eslint-enable linebreak-style */
+    /* eslint-enable linebreak-style */
 
-const expect = [
-  ["text", "\n\n"],
-  ["opentagstart", { name: "moo", attributes: {} }],
-  ["opentag", {
-    name: "moo",
-    attributes: {
-      a: "12\n        3",
-    },
-    isSelfClosing: false,
-  }],
-  ["text", "\n  abc\n  def\r\n  ghi\n\n  xx\nxx\n"],
-  ["closetag", {
-    name: "moo",
-    attributes: {
-      a: "12\n        3",
-    },
-    isSelfClosing: false,
-  }],
-  ["text", "\n"],
-];
+    const expect = [
+      ["text", "\n\n"],
+      ["opentagstart", { name: "moo", attributes: {} }],
+      ["opentag", {
+        name: "moo",
+        attributes: {
+          a: "12\n        3",
+        },
+        isSelfClosing: false,
+      }],
+      ["text", "\n  abc\n  def\r\n  ghi\n\n  xx\nxx\n"],
+      ["closetag", {
+        name: "moo",
+        attributes: {
+          a: "12\n        3",
+        },
+        isSelfClosing: false,
+      }],
+      ["text", "\n"],
+    ];
 
-describe("eol handling", () => {
-  test({
-    name: "one chunk",
-    xml,
-    expect,
+    test({
+      name: "one chunk",
+      xml,
+      expect,
+    });
+
+    test({
+      name: "char-by-char",
+      expect,
+      fn(parser) {
+        for (const x of xml) {
+          parser.write(x);
+        }
+        parser.close();
+      },
+    });
   });
 
-  test({
-    name: "char-by-char",
-    expect,
-    fn(parser) {
-      for (const x of xml) {
-        parser.write(x);
-      }
-      parser.close();
-    },
+  describe("bad start", () => {
+    const xml = `
+<?xml version="1.0" encoding="utf-8"?><doc/>`;
+    const expect = [
+      [
+        "error",
+        "2:6: an XML declaration must be at the start of the document.",
+      ],
+      ["opentagstart", {
+        name: "doc",
+        attributes: {},
+      }],
+      ["opentag", {
+        name: "doc",
+        attributes: {},
+        isSelfClosing: true,
+      }],
+      ["closetag", {
+        name: "doc",
+        attributes: {},
+        isSelfClosing: true,
+      }],
+    ];
+
+    describe("with nl as eol", () => {
+      test({
+        name: "one chunk",
+        xml,
+        expect,
+      });
+
+      test({
+        name: "char-by-char",
+        expect,
+        fn(parser) {
+          for (const x of xml) {
+            parser.write(x);
+          }
+          parser.close();
+        },
+      });
+    });
+
+    describe("with crnl as eol", () => {
+      const crnl = xml.replace(/\n/g, "\r\n");
+      test({
+        name: "one chunk",
+        xml: crnl,
+        expect,
+      });
+
+      test({
+        name: "char-by-char",
+        expect,
+        fn(parser) {
+          for (const x of crnl) {
+            parser.write(x);
+          }
+          parser.close();
+        },
+      });
+    });
   });
 });
