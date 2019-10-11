bed38a841913ba5c7917624f1f36254560042936
Louis-Dominique Dubeau <ldd@lddubeau.com>
2019-10-11 07:17:27 -0400

    fix: fix a bug in EOL handling
    A non-NL character acting as EOL and seen by captureTo would cause a crash in
    some cases.

--- test/eol-handling.js
+++ test/eol-handling.js
@@ -58,6 +58,242 @@ describe("eol handling", () => {
     });
   });
 
+  // The comprehensive test is meant to have EOL in all possible contexts.
+  describe("comprehensive", () => {
+    const nl = `\
+<?xml
+version
+=
+"1.0"
+encoding
+=
+"utf-8"
+standalone
+=
+"no"
+?>
+<!DOCTYPE
+root
+[
+<!--
+I'm a test.
+-->
+<?
+I'm a test.
+?>
+<!NOTATION
+not1
+SYSTEM
+"]>"
+>
+]>
+<!--
+-->
+<?fnord
+?>
+<moo
+a
+=
+"12
+        3"
+b
+=
+"
+z
+"
+>
+abc
+&#xD;
+
+<!--
+-->
+<?fnord
+?>
+<abc
+a
+=
+"bc"
+/>
+</moo
+>
+`;
+    const xmlDeclEnd = nl.indexOf("?>");
+
+    const expect = [
+      ["text", "\n"],
+      ["doctype", `
+root
+[
+<!--
+I'm a test.
+-->
+<?
+I'm a test.
+?>
+<!NOTATION
+not1
+SYSTEM
+"]>"
+>
+]`],
+      ["text", "\n"],
+      ["comment", "\n"],
+      ["text", "\n"],
+      ["processinginstruction", { target: "fnord", body: "" }],
+      ["text", "\n"],
+      ["opentagstart", { name: "moo", attributes: {} }],
+      ["opentag", {
+        name: "moo",
+        attributes: {
+          a: "12\n        3",
+          b: "\nz\n",
+        },
+        isSelfClosing: false,
+      }],
+      ["text", `
+abc
+\r
+
+`],
+      ["comment", "\n"],
+      ["text", "\n"],
+      ["processinginstruction", { target: "fnord", body: "" }],
+      ["text", "\n"],
+      ["opentagstart", { name: "abc", attributes: {} }],
+      ["opentag", {
+        name: "abc",
+        attributes: {
+          a: "bc",
+        },
+        isSelfClosing: true,
+      }],
+      ["closetag", {
+        name: "abc",
+        attributes: {
+          a: "bc",
+        },
+        isSelfClosing: true,
+      }],
+      ["text", "\n"],
+      ["closetag", {
+        name: "moo",
+        attributes: {
+          a: "12\n        3",
+          b: "\nz\n",
+        },
+        isSelfClosing: false,
+      }],
+      ["text", "\n"],
+    ];
+
+    describe("nl", () => {
+      test({
+        name: "one chunk",
+        xml: nl,
+        expect,
+      });
+
+      test({
+        name: "char-by-char",
+        expect,
+        fn(parser) {
+          for (const x of nl) {
+            parser.write(x);
+          }
+          parser.close();
+        },
+      });
+    });
+
+    describe("cr", () => {
+      const cr = nl.replace(/\n/g, "\r");
+
+      test({
+        name: "one chunk",
+        xml: cr,
+        expect,
+      });
+
+      test({
+        name: "char-by-char",
+        expect,
+        fn(parser) {
+          for (const x of cr) {
+            parser.write(x);
+          }
+          parser.close();
+        },
+      });
+    });
+
+    describe("crnl", () => {
+      const crnl = nl.replace(/\n/g, "\r\n");
+
+      test({
+        name: "one chunk",
+        xml: crnl,
+        expect,
+      });
+
+      test({
+        name: "char-by-char",
+        expect,
+        fn(parser) {
+          for (const x of crnl) {
+            parser.write(x);
+          }
+          parser.close();
+        },
+      });
+    });
+
+    describe("nel", () => {
+      // We have to switch the EOL characters after the XML declaration.
+      const nel = nl.slice(0, xmlDeclEnd).replace("1.0", "1.1") +
+            nl.slice(xmlDeclEnd).replace(/\n/g, "\u0085");
+
+      test({
+        name: "one chunk",
+        xml: nel,
+        expect,
+      });
+
+      test({
+        name: "char-by-char",
+        expect,
+        fn(parser) {
+          for (const x of nel) {
+            parser.write(x);
+          }
+          parser.close();
+        },
+      });
+    });
+
+    describe("ls", () => {
+      // We have to switch the EOL characters after the XML declaration.
+      const ls = nl.slice(0, xmlDeclEnd).replace("1.0", "1.1") +
+            nl.slice(xmlDeclEnd).replace(/\n/g, "\u2028");
+
+      test({
+        name: "one chunk",
+        xml: ls,
+        expect,
+      });
+
+      test({
+        name: "char-by-char",
+        expect,
+        fn(parser) {
+          for (const x of ls) {
+            parser.write(x);
+          }
+          parser.close();
+        },
+      });
+    });
+  });
+
   describe("bad start", () => {
     const xml = `
 <?xml version="1.0" encoding="utf-8"?><doc/>`;
