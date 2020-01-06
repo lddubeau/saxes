530e3b9e91161b5b27c329408d2242b286c556d5
Louis-Dominique Dubeau <ldd@lddubeau.com>
2020-01-06 11:46:35 -0500

    build: improve linting setup
    Consolidate the configuration files into a single top one. Provide better
    commands in package.json. Fix new reports.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,33 @@ module.exports = {
   env: {
     node: true,
   },
+  overrides: [{
+    files: [
+      "lib/**/*.js",
+    ],
+    rules: {
+      "no-continue": "off",
+      // We use constant conditions quite often, for optimization reasons.
+      "no-constant-condition": "off",
+    },
+  }, {
+    files: [
+      "test/**/*.js",
+    ],
+    env: {
+      mocha: true,
+    },
+    rules: {
+      "no-unused-expressions":
+      ["off", "Lots of false positivites due to chai."],
+    },
+  },{
+    files: [
+      "misc/**/*.js",
+    ],
+    env: {
+      browser: true,
+      node: false,
+    },
+  }],
 };
--- examples/dump-events.js
+++ examples/dump-events.js
@@ -1,5 +1,7 @@
 "use strict";
 
+/* eslint-disable no-console */
+
 const fs = require("fs");
 const saxes = require("../lib/saxes");
 
