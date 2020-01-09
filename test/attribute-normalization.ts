be48aae005542f27efc7330df4d8b90b79163fc4
Louis-Dominique Dubeau <ldd@lddubeau.com>
2020-01-09 06:55:44 -0500

    test: add the attribute normalization test
    Apparently that test was missed when the attribute normalization fix was put
    in. Oops.

---
+++ test/attribute-normalization.ts
@@ -0,0 +1,26 @@
+import { test } from "./testutil";
+
+test({
+  name: "attribute normalization",
+  // \n\r is converted to \n\n internally. The reverse would be converted to
+  // only \n.
+  // eslint-disable-next-line @typescript-eslint/quotes
+  xml: `<root attr1="&#xD;&#x20;&#xA;&#x9;" attr2="\n\r\ta  b"/>`,
+  expect: [
+    ["opentagstart", {
+      name: "root",
+      attributes: {},
+    }],
+    ["opentag", {
+      name: "root",
+      attributes: { attr1: "\r \n\t", attr2: "   a  b" },
+      isSelfClosing: true,
+    }],
+    ["closetag", {
+      name: "root",
+      attributes: { attr1: "\r \n\t", attr2: "   a  b" },
+      isSelfClosing: true,
+    }],
+  ],
+  opt: { },
+});
